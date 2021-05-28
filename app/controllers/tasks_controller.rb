4639925d1001135efb0882964d4ba73ba31d932f
taro.kimura <ken54264232ken@gmail.com>
2021-05-28 21:49:19 +0000

    kadai-tasklist7

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,9 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
